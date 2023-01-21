d7ea50f6f8c4dabaebe00844b2e63eb14ed29000
Marco Pacifico <marco.pacifico@gmail.com>
2023-01-21 11:30:00 -0600

    back where I started for real this time

--- components/DarkToggle.js
+++ components/DarkToggle.js
@@ -1,43 +1,51 @@
-import { useEffect } from "react";
-
-const setLight = () => {
-    localStorage.setItem("theme","light");
-    document.documentElement.setAttribute("data-theme","light");
-}
-
-const setDark = () => {
-    localStorage.setItem("theme","dark");
-    document.documentElement.setAttribute("data-theme","dark");
-}
-
-const toggleTheme = (e) => {
-    e.target.checked ? setDark() : setLight();
-}
-
-if (typeof window !== 'undefined') {
-    // Perform localStorage action
-    const storedTheme = localStorage.getItem("theme");
-
-    const prefersDark = 
-        window.matchMedia &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-    const defaultDark = 
-        storedTheme === "dark" ||
-        (storedTheme === null && prefersDark)
-
-    defaultDark && setDark();
-}
+import { useEffect, useState } from "react";
 
 
 export default function DarkToggle() {
+
+    const [hasMounted, setHasMounted] = useState(false);
+
+    useEffect(()=> {
+        setHasMounted(true);
+    },[]);
+
+    if (!hasMounted) {
+        return null;
+    }
+
+    const setLight = () => {
+        localStorage.setItem("theme","light");
+        document.documentElement.setAttribute("data-theme","light");
+    }
+    
+    const setDark = () => {
+        localStorage.setItem("theme","dark");
+        document.documentElement.setAttribute("data-theme","dark");
+    }
+    
+    const toggleTheme = (e) => {
+        e.target.checked ? setDark() : setLight();
+    }
+    
+    const storedTheme = localStorage.getItem("theme");
+    
+    const prefersDark = 
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches;
+    
+    const defaultDark = 
+        storedTheme === "dark" ||
+        (storedTheme === null && prefersDark)
+    
+    defaultDark && setDark();
+
     return (
         <label htmlFor="checkbox">
             <input
                 type="checkbox"
                 id="checkbox"
                 onChange={toggleTheme}
-                
+                defaultChecked={defaultDark}
             />
         </label>
     );
