867f414334b6cefefdc983a88f50c10e963ebc70
Marco Pacifico <marco.pacifico@gmail.com>
2023-01-21 10:42:03 -0600

    attempting fix for darkToggle state

--- components/DarkToggle.js
+++ components/DarkToggle.js
@@ -37,7 +37,7 @@ export default function DarkToggle() {
         storedTheme === "dark" ||
         (storedTheme === null && prefersDark)
     
-    defaultDark && setDark();
+    defaultDark ? setDark() : setLight();
 
     return (
         <label htmlFor="checkbox">
